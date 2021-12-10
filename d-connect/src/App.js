03446592f1b12f490a6aa0f613a9489800c6258b
vikas hiwase <vikas.hiwase20@gmail.com>
2021-12-10 19:10:04 +0530

    Redux setup

--- d-connect/src/App.js
+++ d-connect/src/App.js
@@ -1,5 +1,10 @@
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./redux/actions/authAction";
+import { logoutUser } from "./redux/actions/authAction";
+
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -8,6 +13,27 @@ import Landing from "./components/Landing";
 import Navbar from "./components/Navbar";
 import Register from "./auth/Register";
 import Login from "./auth/Login";
+
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // TODO: Clear current profile
+
+    // Redirect to login
+    window.location.href = "/login";
+  }
+}
 
 function App() {
   return (
